4a37bc92bc2e5b096abef23df982a20e72e8d1b1
GOWxx <sunbowen523@gmail.com>
2022-06-13 16:04:51 +0800

    解决依赖不一致导致项目无法启动的问题
    临时解决：以配套项目的 `package.json` 和 `package-lock.json` 替换本项目，重新安装依赖

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,71 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    index: './src/index.tsx'
+    index: "./src/index.tsx",
   },
   module: {
     rules: [
-      // 解析 TypeScript
       {
         test: /\.(tsx?|jsx?)$/,
-        use: 'ts-loader',
-        exclude: /(node_modules|tests)/
+        use: "ts-loader",
+        exclude: /(node_modules|tests)/,
       },
-      // 解析 css
       {
         test: /\.css$/i,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-        ]
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
       },
-      // 解析 less
       {
         test: /\.less$/i,
         use: [
-          { loader: 'style-loader' },
+          { loader: "style-loader" },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              module: {
+              modules: {
                 mode: (resourcePath) => {
                   if (/pure.css$/i.test(resourcePath)) {
-                    return 'pure';
+                    return "pure";
                   }
                   if (/global.css$/i.test(resourcePath)) {
-                    return 'global';
+                    return "global";
                   }
-                  return 'local';
-                }
-              }
-            }
+                  return "local";
+                },
+              },
+            },
           },
-          { loader: 'less-loader' },
+          { loader: "less-loader" },
         ],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.less', 'css'],
-    // 设置别名
+    extensions: [".tsx", ".ts", ".js", ".less", "css"],
     alias: {
-      utils: path.join(__dirname, 'src/utils/'),
-      components: path.join(__dirname, 'src/components/'),
-      apis: path.join(__dirname, 'src/apis/'),
-      hooks: path.join(__dirname, 'src/hooks/'),
-      store: path.join(__dirname, 'src/store/'),
-    }
+      utils: path.join(__dirname, "src/utils/"),
+      components: path.join(__dirname, "src/components/"),
+      apis: path.join(__dirname, "src/apis/"),
+      hooks: path.join(__dirname, "src/hooks/"),
+      store: path.join(__dirname, "src/store/"),
+    },
   },
-  devtool: 'inline-source-map',
-  // 3000 端口打开网页
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
     port: 3000,
     hot: true,
   },
-  // 默认输出
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  // 指定模板 html
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
