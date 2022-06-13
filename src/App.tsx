456f8d085fbc9b274124cda71dfb4a271d471a8c
GOWxx <sunbowen523@gmail.com>
2022-06-13 18:42:07 +0800

    `jest`生成快照`snapShot`; vscode 插件 `jest` 配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Button } from "antd";
+import Title from "components/Ttile";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
-      <Button>点我</Button>
+      <h1>jest learning</h1>
+      <section>
+        <Title type="small" title="小字" />
+        <Title type="large" title="大字" />
+      </section>
     </div>
   );
 };
